15b402fae8c94ae2771f389d981f777925af390b
Justin Wyer <justin@lifeasageek.com>
2016-10-03 15:35:16 +0200

    Update Xamarin forms

--- CodeFest/CodeFest/App.cs
+++ CodeFest/CodeFest/App.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using CodeFest.Query;
 using Xamarin.Forms;
 
 namespace CodeFest
@@ -12,19 +12,7 @@ namespace CodeFest
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new QueryPage();
         }
 
         protected override void OnStart()
---
+++ CodeFest/CodeFest/Query/QueryPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CodeFest.Query
+{
+    public class QueryPage : ContentPage
+    {
+        public QueryPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label
+                    {
+                        Text = "Hello ContentPage",
+                        HorizontalTextAlignment = TextAlignment.Center
+                    },
+                    new Entry
+                    {
+                        
+                    }
+                }
+            };
+        }
+    }
+}
