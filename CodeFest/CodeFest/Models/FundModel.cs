f83f5f6edb19a36fe336f86c6ac305b08d111068
Tyrone Naidoo <tyrone.naidoo@rmb.co.za>
2016-10-06 11:52:54 +0200

    Added the fund model

---
+++ CodeFest/CodeFest/Models/FundModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFest.Models
+{
+    class FundModel
+    {
+        public string Client { get; set; }
+        public string LegalPersonaFund { get; set; }
+        public string FundName { get; set; }
+        public string RegNumberFund { get; set; }
+    }
+}
