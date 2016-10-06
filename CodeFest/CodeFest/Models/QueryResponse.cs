7ed7251a2886150912050f9643c5a62ed25f5b50
Tyrone Naidoo <tyrone.naidoo@rmb.co.za>
2016-10-07 00:24:53 +0200

    Revert "Deleted unused code"
    This reverts commit deff80cf4299c8f83e684f19ae8c5bdc3b9cb37c.

--- CodeFest/CodeFest/Models/QueryResponse.cs
+++ CodeFest/CodeFest/Models/QueryResponse.cs
@@ -9,8 +9,15 @@ namespace CodeFest.Models
 {
     class QueryResponse
     {
+//        public Intent intent;
+
         [JsonProperty(PropertyName = "intent")]
         public string intentName;
     }
 
+//    internal class Intent
+//    {
+//
+//        public string intentName;
+//    }
 }
